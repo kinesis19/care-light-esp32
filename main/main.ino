edb2eeb8051c489db3ef139ab46aba7448b88c11
kinesis19 <lordkinesis@naver.com>
2025-06-01 00:51:44 +0900

    :sparkles: Feat: pulse sensor feature

--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,29 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <ESP32Servo.h>
+#define USE_ARDUINO_INTERRUPTS true
+#include <PulseSensorPlayground.h>
 
 // Wi-Fi 설정
-const char* ssid = "융합씽킹랩 2"; // 사용하는 Wi-Fi SSID로 변경
-// const char* password = ""; // 개방형 네트워크의 경우 비밀번호 필요 없음
+const char* ssid = "융합씽킹랩 2";
 
-// LED 설정 (기존 코드 유지)
+// LED 설정
 #ifndef LED_BUILTIN
   #define LED_BUILTIN 2
 #endif
 
 // 서보 모터 설정
 Servo myservo;
-int servoPin = 17;
-int currentServoAngle = 90; // 서보 모터 초기값: 90도
+const int SERVO_PIN = 17;
+int currentServoAngle = 90;
 
-WebServer server(80); // HTTP 서버 포트 80
+// Pulse Sensor 설정
+const int PULSE_SENSOR_PIN = 4;
+const int PULSE_SENSOR_THRESHOLD = 550;
+PulseSensorPlayground pulseSensor;
+int currentBPM = 0;
 
+WebServer server(80);
 
 void handleRoot() {
   String html = "<h1>ESP32 Web Server</h1>";
@@ -25,6 +31,7 @@ void handleRoot() {
   html += "<p><a href=\"/ledoff\"><button>Turn LED OFF</button></a></p>";
   html += "<p>Current Servo Angle: " + String(currentServoAngle) + "</p>";
   html += "<p><form action='/set_servo' method='get'>Angle: <input type='number' name='angle' min='0' max='180' value='" + String(currentServoAngle) + "'> <input type='submit' value='Set Servo'></form></p>";
+  html += "<p>Current BPM: " + String(currentBPM) + "</p>";
   server.send(200, "text/html", html);
 }
 
@@ -40,24 +47,6 @@ void handleLedOff() {
   Serial.println("LED turned OFF");
 }
 
-void handleEspInfo() {
-  String deviceName = "CareLight_ESP32_" + WiFi.macAddress().substring(9);
-  deviceName.replace(":", "");
-  String status = "Online";
-  String currentSsid = WiFi.SSID();
-
-  String jsonResponse = "{";
-  jsonResponse += "\"name\":\"" + deviceName + "\",";
-  jsonResponse += "\"status\":\"" + status + "\",";
-  jsonResponse += "\"wifi_ssid\":\"" + currentSsid + "\",";
-  jsonResponse += "\"servo_angle\":" + String(currentServoAngle);
-  jsonResponse += "}";
-
-  server.send(200, "application/json", jsonResponse);
-  Serial.println("Sent ESP32 info (with servo angle) to client.");
-}
-
-// 서보 모터 각도 설정 핸들러
 void handleSetServo() {
   String angleArg = server.arg("angle");
   if (angleArg == "") {
@@ -65,18 +54,15 @@ void handleSetServo() {
     Serial.println("Set servo failed: Angle parameter missing");
     return;
   }
-
   int newAngle = angleArg.toInt();
   if (newAngle < 0 || newAngle > 180) {
     server.send(400, "text/plain", "Angle out of range (0-180)");
     Serial.println("Set servo failed: Angle out of range");
     return;
   }
-
-  myservo.write(newAngle); // 서보 모터 각도 설정
-  currentServoAngle = newAngle; // 현재 각도 업데이트
+  myservo.write(newAngle);
+  currentServoAngle = newAngle;
   delay(15);
-
   String responseMessage = "Servo angle set to " + String(newAngle);
   server.send(200, "text/plain", responseMessage);
   Serial.println(responseMessage);
@@ -88,21 +74,46 @@ void handleGetServoAngle() {
     Serial.println("Sent current servo angle: " + String(currentServoAngle));
 }
 
+void handleEspInfo() {
+  String deviceName = "CareLight_ESP32_" + WiFi.macAddress().substring(9);
+  deviceName.replace(":", "");
+  String status = "Online";
+  String currentSsid = WiFi.SSID();
+
+  String jsonResponse = "{";
+  jsonResponse += "\"name\":\"" + deviceName + "\",";
+  jsonResponse += "\"status\":\"" + status + "\",";
+  jsonResponse += "\"wifi_ssid\":\"" + currentSsid + "\",";
+  jsonResponse += "\"servo_angle\":" + String(currentServoAngle) + ",";
+  jsonResponse += "\"bpm\":" + String(currentBPM);
+  jsonResponse += "}";
+
+  server.send(200, "application/json", jsonResponse);
+  Serial.println("Sent ESP32 info (with servo and BPM) to client.");
+}
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
-  myservo.attach(servoPin); // 서보 핀 연결
-  myservo.write(currentServoAngle); // 초기 각도로 설정
+  myservo.attach(SERVO_PIN);
+  myservo.write(currentServoAngle);
   Serial.println("Servo attached and set to initial position.");
+
+  pulseSensor.analogInput(PULSE_SENSOR_PIN);
+  pulseSensor.setThreshold(PULSE_SENSOR_THRESHOLD);
+  if (pulseSensor.begin()) {
+    Serial.println("PulseSensor Playground object created!");
+  } else {
+    Serial.println("PulseSensor Playground object NOT created!");
+  }
 
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  WiFi.begin(ssid); // 개방형 네트워크 연결
+  WiFi.begin(ssid);
 
   int attempts = 0;
   while (WiFi.status() != WL_CONNECTED && attempts < 20) {
@@ -123,14 +134,14 @@ void setup() {
     Serial.print("ESP32 IP Address: ");
     Serial.println(WiFi.localIP());
 
-    server.on("/", HTTP_GET, handleRoot); // 루트 경로 핸들러
-    server.on("/ledon", HTTP_GET, handleLedOn); // LED ON 핸들러
-    server.on("/ledoff", HTTP_GET, handleLedOff); // LED OFF 핸들러
-    server.on("/info", HTTP_GET, handleEspInfo); // ESP32 정보 제공 핸들러 (서보 각도 포함)
-    server.on("/set_servo", HTTP_GET, handleSetServo); // 서보 각도 설정 핸들러
-    server.on("/get_servo_angle", HTTP_GET, handleGetServoAngle); // 서보 각도만 가져오는 핸들러
+    server.on("/", HTTP_GET, handleRoot);
+    server.on("/ledon", HTTP_GET, handleLedOn);
+    server.on("/ledoff", HTTP_GET, handleLedOff);
+    server.on("/info", HTTP_GET, handleEspInfo);
+    server.on("/set_servo", HTTP_GET, handleSetServo);
+    server.on("/get_servo_angle", HTTP_GET, handleGetServoAngle);
 
-    server.begin(); // 웹 서버 시작
+    server.begin();
     Serial.println("HTTP server started");
   } else {
     Serial.println("DEBUG: Entered 'ELSE (not WL_CONNECTED)' block.");
@@ -144,6 +155,17 @@ void setup() {
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
-    server.handleClient(); // 클라이언트 요청 처리
+    server.handleClient(); // 웹 서버 클라이언트 요청 처리
   }
+
+  if (pulseSensor.sawStartOfBeat()) { 
+    currentBPM = pulseSensor.getBeatsPerMinute();
+    // BPM 값이 유효할 때만
+    if (currentBPM > 0) {
+        Serial.print("BPM: ");
+        Serial.println(currentBPM);
+    }
+  }
+
+  delay(20);
 }
